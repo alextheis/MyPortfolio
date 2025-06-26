d80991883e266b69ec13831d936b58858ce1cd15
[Alexander Theis] <[yzy.amt03@gmail.com]>
2025-06-26 16:26:05 -0400

    finished Css, started Theme Toggle and Background Effects

---
+++ beautiful-portfolio/src/components/StarBackground.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+// id, size, x, y, opacity, animationDuration
+
+export const StarBackground = () => {
+        const[stars, setStars] = useState([])
+
+        const generateStars = () => {
+            const numberOfStars = Math.floor(
+                (window.innerWidth * window.innerHeight) / 10000
+            )
+        }
+
+    return <div> </div>;
+}
