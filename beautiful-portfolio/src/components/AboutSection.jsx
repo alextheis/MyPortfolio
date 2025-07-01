c91602afafcb6573e15ce70f27f11d8b948ad49a
[Alexander Theis] <[yzy.amt03@gmail.com]>
2025-07-01 17:52:44 -0400

    finished the navbar and hero sections, started the about me section

---
+++ beautiful-portfolio/src/components/AboutSection.jsx
@@ -0,0 +1,12 @@
+
+
+export const AboutSection = () => {
+    return <section id="about" className="ppy-24 px-4 relative">
+        {" "}
+        <div className="contailneer mx-auto max-x-5xl">
+            <h2>
+              About <span className="text-primary">Me</span>  
+            </h2>
+        </div>
+    </section>
+}
--- beautiful-portfolio/src/components/Navbar.jsx
+++ beautiful-portfolio/src/components/Navbar.jsx
@@ -52,13 +52,20 @@ export const Navbar = () => {
                 ))}
             </div>
             {/* mobile nav */}
+
+            <button onClick={() => setIsMenuOpen((prev) => !prev) } className="me:hidden p-2 text-foreground z-50" aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}>
+                {" "}
+                {isMenuOpen ? <X size={24} /> : <Menu size={24}/>}{" "} 
+            </button>
             <div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
                 "transition-all duration-300 md:hidden",
                 isMenuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
             )}>
-            <div className="hidden md:flex space-x-8">
+            <div className="flex flex-col space-y-8 text-xl">
                 {navItems.map((item, key) => (
-                    <a key={key} href={item.href} className="text-foreground/80 hover:text-primary transition colors duration-300">
+                    <a key={key} href={item.href} className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                        onClick={() => setIsMenuOpen(false)}
+                    >
                         {item.name}
                     </a>
                 ))}
